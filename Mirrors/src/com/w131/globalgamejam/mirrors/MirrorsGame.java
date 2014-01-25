9a7c6f7c53250ac6c4cfd2c58dfaa8ce9b896266
Joshua Luke <joshuadluke@gmail.com>
2014-01-24 23:56:43 -0500

    Basic Movement

--- Mirrors/src/com/w131/globalgamejam/mirrors/MirrorsGame.java
+++ Mirrors/src/com/w131/globalgamejam/mirrors/MirrorsGame.java
@@ -1,54 +1,58 @@
 package com.w131.globalgamejam.mirrors;
 
-import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.w131.globalgamejam.mirrors.screens.GameScreen;
 
-public class MirrorsGame implements ApplicationListener {
+public class MirrorsGame extends Game {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
+	
+	private Controller player;
 	
 	@Override
-	public void create() {		
+	public void create() {
+		// Default Init stuff
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		
-		camera = new OrthographicCamera(1, h/w);
+		camera = new OrthographicCamera(w, h);
+		setScreen(new GameScreen(0));
+		camera.setToOrtho(true);
 		batch = new SpriteBatch();
 		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		// My Init
+		Gdx.input.setInputProcessor(new KeyHandler());
+		player = new Controller();
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
-		texture.dispose();
+	}
+	
+	public void tick(float delta) {
+		player.tick(delta);
 	}
 
 	@Override
-	public void render() {		
+	public void render() {
+		// I like having a tick function...
+		tick(Gdx.graphics.getDeltaTime());
+		
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		sprite.draw(batch);
+		
+		// BEGIN MY DRAWING
+		player.render(batch);
+		// END MY DRAWING
+		
 		batch.end();
 	}
 
