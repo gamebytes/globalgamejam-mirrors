540eaca4e8f1e0e6e512b78c86a666a72c0ec6f0
Joshua Luke <joshuadluke@gmail.com>
2014-01-25 11:13:47 -0500

    More improved collisions

--- Mirrors/src/com/w131/globalgamejam/mirrors/Controller.java
+++ Mirrors/src/com/w131/globalgamejam/mirrors/Controller.java
@@ -5,7 +5,6 @@ import java.util.LinkedList;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 import com.w131.globalgamejam.mirrors.screens.GameScreen;
 
 public class Controller {
